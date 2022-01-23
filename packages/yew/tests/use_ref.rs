88c7f8af231dcf9e6d0e18ee6343589b97b079b3
Kaede Hoshikawa <futursolo@icloud.com>
2022-01-23 23:05:10 +0900

    Fix test.

--- packages/yew/tests/use_ref.rs
+++ packages/yew/tests/use_ref.rs
@@ -1,13 +1,15 @@
 mod common;
 
 use common::obtain_result;
+use gloo::timers::future::sleep;
+use std::time::Duration;
 use wasm_bindgen_test::*;
 use yew::prelude::*;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn use_ref_works() {
+async fn use_ref_works() {
     #[function_component(UseRefComponent)]
     fn use_ref_comp() -> Html {
         let ref_example = use_ref(|| 0);
@@ -15,11 +17,11 @@ fn use_ref_works() {
 
         {
             let ref_example = ref_example.clone();
-            use_effect(|| {
+            use_effect(move || {
                 let should_render = ref_example.with_mut(|m| {
                     *m += 1;
 
-                    *m < 5
+                    *m == 1
                 });
 
                 if should_render {
@@ -33,7 +35,7 @@ fn use_ref_works() {
         html! {
             <div>
                 {"The test output is: "}
-                <div id="result">{ref_example.with(|m| *m) > 4}</div>
+                <div id="result">{ref_example.with(|m| *m) != 0}</div>
                 {"\n"}
             </div>
         }
@@ -42,6 +44,8 @@ fn use_ref_works() {
     yew::start_app_in_element::<UseRefComponent>(
         gloo_utils::document().get_element_by_id("output").unwrap(),
     );
+
+    sleep(Duration::from_millis(10)).await;
 
     let result = obtain_result();
     assert_eq!(result.as_str(), "true");
